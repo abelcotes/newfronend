25ec921fa304173b58a83c9328ecd0fd0f8d52f4
mjpulido1983 <85773782+mjpulido1983@users.noreply.github.com>
2021-10-27 10:01:51 -0500

    app ajustado

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import './App.css';
+import {
+  BrowserRouter,
+  Switch,
+  Route
+} from "react-router-dom";
+import Prenda from './components/Prenda/Prenda';
+import User from './components/User/User';
+import Login from './components/Login/Login';
+import Register from './components/Register/Register';
 
-      
+
+import PaginaNoEncontrada from './components/PaginaNoEncontrada/PaginaNoEncontrada';
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Switch>
       <Route path="/users" exact>
           <User />
          </Route>
@@ -22,8 +39,7 @@
         <Route path="/register" exact>
           <Register/>
         </Route>
-        
-
+      
       </Switch>
     </BrowserRouter>
   );
